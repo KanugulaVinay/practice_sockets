b535aaf68ffc7ecece6e042f66917ca1a99deab7
vinay kanugula <vinaykanugula17@gmail.com>
2024-01-27 20:07:40 +0530

    first commit

---
+++ server/socket/sockets.js
@@ -0,0 +1,33 @@
+import MessageController from "./controller/MessageController.js";
+import RoomController from "./controller/RoomController.js";
+import TypingController from "./controller/TypingController.js";
+import fs from "fs";
+
+const sockets = (socket) => {
+    const typingController = new TypingController(socket);
+    const roomController = new RoomController(socket);
+    const messageController = new MessageController(socket);
+
+    socket.on("send-message", messageController.sendMessage);
+    socket.on("typing-started", typingController.typingStarted);
+    socket.on("typing-stoped", typingController.typingStoped);
+    socket.on("join-room", roomController.joinRoom)
+    socket.on("new-room-created", roomController.newRoomCreated)
+    socket.on("room-removed", roomController.roomRemoved)
+    socket.on("upload", ({ data, roomId }) => {
+        fs.writeFile(
+          "upload/" + "test.png",
+          data,
+          { encoding: "base64" },
+          () => {}
+        );
+    
+        socket.to(roomId).emit("uploaded", { buffer: data.toString("base64") });
+      });
+    
+      socket.on("disconnect", (socket) => {
+        console.log("User left.");
+    });
+}
+
+export default sockets;
